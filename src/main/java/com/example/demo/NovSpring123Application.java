ee8919501778fb7ec7c2e433e202814369b77aa2
psunkana <67285784+psunkana@users.noreply.github.com>
2021-11-01 06:50:40 -0700

    Pushed this project at first time in git

---
+++ src/main/java/com/example/demo/NovSpring123Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovSpring123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovSpring123Application.class, args);
+	}
+
+}
